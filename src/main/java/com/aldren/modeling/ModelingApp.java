c3c60290d196a3b0163b20a56046003c3e92c2c3
iamaldren <aldrenbobiles@gmail.com>
2019-11-12 19:44:46 +0800

    Initial Commit

---
+++ src/main/java/com/aldren/modeling/ModelingApp.java
@@ -0,0 +1,13 @@
+package com.aldren.modeling;
+
+/**
+ * Hello world!
+ *
+ */
+public class ModelingApp
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/aldren/modeling/ModelingAppTest.java
@@ -0,0 +1,20 @@
+package com.aldren.modeling;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class ModelingAppTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
